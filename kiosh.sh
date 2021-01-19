48a76ed04b660c95164d1608ac24703e6a21cded
Pickles <2606634+carvsdriver@users.noreply.github.com>
2021-01-19 09:00:40 -0500

    initial commit

---
+++ kiosh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#
+# this script launches kios mode with chromium to display the Taplist
+#
+/bin/sleep 3
+
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost &
